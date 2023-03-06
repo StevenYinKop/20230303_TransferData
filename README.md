5061f20663ff758b270c03de04a22c28b38a9e57
Steven.Yin <yinzifancn@gmail.com>
2023-03-07 00:03:52 +0800

    add tons of comments

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # 20230303_TransferData
+
+## exe文件的使用方法
+
+### 文件位置：main.exe
+
+1. 双击打开main.exe，页面提示你输入索引文件的路径：
+```shell
+请输入索引文件的路径，按回车键确认(默认路径为：<main.exe所在的文件夹>\工作目录\index.xls):
+```
+如果你什么都不填写，那么就会默认去找(<main.exe所在的文件夹>\工作目录\index.xls)
+
+2. 请保证所有待读取的数据文件，和索引文件在同一个文件夹下。
+3. 回车后，查看执行结果：
+
+
+## python源代码的时候方法
+1. 找到`main.py`文件，然后打开命令行
+```shell
+cd <main.py所在的文件夹>
+pip install openpyxl xlrd # 这个命令只需要在代码第一次运行的时候执行。
+python main.py
+```
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
-
+# 解析xlsx所用的库
 from openpyxl import load_workbook, Workbook
+# 解析xls所用的库
 from xlrd import open_workbook
 
 # 定义标题名称
@@ -16,14 +17,19 @@ title_field_index = '字段名称列/行号'
 title_data_index = '数据列/行号'
 title_is_horizontal_or_vertical = '横表/竖表'
 
+# 解析当前python脚本所在的路径
 script_path = os.path.abspath(__file__)
+# 默认的数据路径，设定为：/python脚本所在路径/工作目录/
 base_dir = f'{os.path.dirname(script_path)}{os.sep}工作目录'
 
+# 定义输出的文件名
 output_filename = f'{base_dir}{os.sep}total.xlsx'
+# 定义错误日志的文件名
 error_filename = f'{base_dir}{os.sep}错误日志.txt'
+# 定义收集错误日志所用的列表
 error_list = []
-error_index_count = 0
 
+# 定义索引文件的标题栏，后续用来方便读取索引文件的数据
 title_list = [
     title_author,
     title_file_path,
@@ -37,7 +43,7 @@ title_list = [
     title_is_horizontal_or_vertical
 ]
 
-# 声明一个结果集，后续的结果都会存在这个列表中
+"""声明一个结果集，后续的结果都会存在这个列表中"""
 data_list = [
     [
         '数据采集人',
@@ -47,28 +53,43 @@ data_list = [
         '数据']
 ]
 
-
+"""
+格式化错误日志，使得错误日志中，每一行都可以有统一的前缀
+类似于：[-----第5行-----]：文件解析异常！
+"""
 def get_format_error_cell(error_str, row_number):
     return f'[-----第{row_number}行-----]：{error_str}'
 
 
+"""
+将错误日志输出到文件中，输出完毕后，终止当前程序
+"""
 def output_error_list():
+    # 如果没有错误日志，那么不执行后续的操作了。
     if len(error_list) == 0:
         return
     error_file = open(error_filename, 'w')
+    # 逐行将错误内容写入文件
     for error_item in error_list:
         error_file.write(f'{error_item}\n')
     print(f"程序执行过程中发现错误，请查看{error_filename}")
     error_file.close()
+    # 退出python程序
     sys.exit()
     # exit()
 
+"""
+格式化索引: 如果传入的"采集起始行/列"和"采集终止行/列"超过了excel中已有的最大长度，则以数据最大长度为准：
 
+如："采集终止行/列"为100，但是数据最多只有50条，那么强制将"采集终止行/列"读取为50
+"""
 def standardize_index(index_row_dict, get_max):
     return max(int(index_row_dict[title_start_at]), 1), \
            min(int(index_row_dict[title_end_at]), get_max())
 
-
+"""
+组装最终输入到结果文件的数据
+"""
 def assemble_data_list_item(field, data, index_dict):
     return [
         index_dict[title_author], # 数据采集人
@@ -79,7 +100,9 @@ def assemble_data_list_item(field, data, index_dict):
     ]
 
 
-# 校验索引文件的格式, 防止索引文件中，输入的数据质量差，使程序崩溃
+"""
+校验索引文件的格式, 防止索引文件中，缺少一些必要的字段，导致程序无法正确执行
+"""
 def validate_index_file_parameters(headers, index_filename):
     print(f'正在检查索引文件"{index_filename}"中的数据有效性...')
     for title_str in title_list:
@@ -88,18 +111,24 @@ def validate_index_file_parameters(headers, index_filename):
     output_error_list()
 
 
+"""
+验证索引文件中的每一个字段是否能够正常读取
+"""
 def validate_index_data(index_dict, row):
     error_sub_list = []
     for title_str in [title_year, title_start_at, title_end_at, title_data_index, title_field_index]:
         try:
+            # 如果年份，起止行等数据，不是数值格式(1,2,3)，而是一些奇怪的值(任意的文字或者字母)，则将错误信息放入错误列表中
             int(float((index_dict[title_str])))
         except Exception as e:
             error_sub_list.append(get_format_error_cell(f' "{title_str}" 不是一个数字格式，无法解析！', row))
     if index_dict[title_is_horizontal_or_vertical] not in ['横表', '竖表']:
         error_sub_list.append(get_format_error_cell(f' "{title_is_horizontal_or_vertical}"中只能填"横表"或者"竖表"！', row))
+        # 如果数据文件不存在，则将错误信息存入错误列表中
     if not os.path.exists(f'{base_dir}{os.sep}{str(index_dict[title_file_path])}'):
         error_sub_list.append(get_format_error_cell(f' "{title_file_path}" 文件不存在，无法解析！', row))
     else:
+        # 检查数据文件中，是否存在相应的Sheet页。
         file_path = f'{base_dir}{os.sep}{str(index_dict[title_file_path])}'
         _, extension = os.path.splitext(file_path)
         if extension == '.xlsx':
@@ -115,30 +144,37 @@ def validate_index_data(index_dict, row):
                 error_sub_list.append(
                     get_format_error_cell(f'{title_file_path}文件中不存在名为{index_dict[title_sheet_name]}的Sheet页，无法解析！', row))
             workbook.release_resources()
+    # 如果错误列表中存在数据，则说明这一条数据是无法正常解析的，将会忽略对该行的操作
     if len(error_sub_list) != 0:
         error_sub_list.append(get_format_error_cell(f'跳过第{row}行...\n', row))
     error_list.extend(error_sub_list)
     return len(error_sub_list) == 0
 
 
-# 定义一个解析数据文件的函数
+"""
+定义一个解析数据文件的函数
+"""
 def parse_data_file(file_path, index_row_dict):
     sub_data_list = []
     print(f'开始处理文件{file_path}...')
     _, extension = os.path.splitext(file_path)
     field_index = int(index_row_dict[title_field_index])
     data_index = int(index_row_dict[title_data_index])
+    # 根据不同的文件名后缀，选择不同的库对excel进行操作
     if extension == '.xlsx':
         print('文件是xlsx格式，使用新excel的解析方式...请稍后...')
         workbook = load_workbook(file_path)
+        # 如果索引文件中指定了Sheet名，那么用他指定的名字，否则选择excel中的第一个sheet页
         sheet = workbook[index_row_dict[title_sheet_name]] \
             if index_row_dict[title_sheet_name] not in [None, ''] \
             else workbook.worksheets[0]
         print(f'文件加载成功，当前操作的sheet页为{sheet.title}')
         if index_row_dict[title_is_horizontal_or_vertical] == '横表':
+            # 横表操作
             start_at, end_at = standardize_index(index_row_dict, lambda: sheet.max_column)
             print(f'以"{index_row_dict[title_is_horizontal_or_vertical]}"'
                   f'的方式解析当前sheet页的第{start_at}列至第{end_at}列...')
+            # 因为是横表，所以逐列对数据进行读取
             for col in range(start_at, end_at + 1):
                 print(f'开始解析第{col}列...')
                 item = assemble_data_list_item(sheet.cell(field_index, col).value,
@@ -147,6 +183,7 @@ def parse_data_file(file_path, index_row_dict):
                 sub_data_list.append(item)
                 print(f'第{col}列解析完毕！, 数据为：{item}')
         elif index_row_dict[title_is_horizontal_or_vertical] == '竖表':
+            # 竖表操作
             start_at, end_at = standardize_index(index_row_dict, lambda: sheet.max_row)
             print(f'以"{index_row_dict[title_is_horizontal_or_vertical]}"'
                   f'的方式解析当前sheet页的第{start_at}行至第{end_at}行...')
@@ -197,7 +234,6 @@ def parse_data_file(file_path, index_row_dict):
 # 定义一个解析索引文件的函数
 def parse_index_file(index_filename):
     _, extension = os.path.splitext(index_filename)
-
     # 如果是xlsx后缀名的文件，走这个if逻辑
     if extension == '.xlsx':
         workbook = load_workbook(index_filename)
@@ -205,8 +241,10 @@ def parse_index_file(index_filename):
         headers = [cell.value for cell in sheet[1]]
         validate_index_file_parameters(headers, index_filename)
         for row in sheet.iter_rows(min_row=2):
+            # 解析索引文件，将索引文件的每一行作为分别保存下来，存入index_row_dict
             index_row_dict = {headers[i]: row[i].value for i in range(len(headers))}
             if validate_index_data(index_row_dict, row[0].row):
+                # 代码能够走到这里，说明索引文件的数据是可以正常被解析到的，那么开始解析数据文件中的数据
                 data_list.extend(
                     parse_data_file(f'{base_dir}{os.sep}{index_row_dict[title_file_path]}', index_row_dict))
 
@@ -250,13 +288,16 @@ def main():
     global error_filename
     default_index_filename = f'{base_dir}{os.sep}index.xls'
     print("欢迎使用数据迁移程序！")
+    # 引导用户输入索引文件的全路径，并且将值存在index_filename中
     index_filename = input(f'请输入索引文件的路径，按回车键确认(默认路径为：{default_index_filename}):\n')
+    # 如果用户什么都没有输入，直接敲了回车，那么就用默认的文件路径
     if index_filename == '':
         index_filename = default_index_filename
     base_dir = f'{os.path.dirname(index_filename)}{os.sep}'
     output_filename = f'{base_dir}{os.sep}total.xlsx'
     error_filename = f'{base_dir}{os.sep}错误日志.txt'
     input(f'请将所有的数据文件，放置在"{base_dir}"文件夹下，按回车键确认\n')
+    # 只有当输入的索引文件真实存在，才会进入到解析逻辑，否则直接报错并且程序结束
     if os.path.exists(index_filename):
         # 解析索引文件
         parse_index_file(index_filename)
